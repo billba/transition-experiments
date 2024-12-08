6d6d30628cc2eb0d7d1d9e9ed7d67bfbf42a8991
Bill Barnes <billba@microsoft.com>
2024-12-08 15:07:16 -0700

    initial experiment

---
+++ README.md
@@ -0,0 +1,12 @@
+# transition-experiments
+
+A bunch of experiments around using transitions in [Excitation](https://www.github.com/billba/excitation)
+
+## getting started
+
+clone this repo then
+
+```zsh
+npm i
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div id="Grid">
+      <div id="Q">Question</div>
+      <div id="A">Answer</div>
+      <div id="C">Citations</div>
+      <div id="R">Review</div>
+    </div>
+  )
+};
+
+export default App;
