20fbef1e3e0f774ce672bf8401d129b3c3ca3955
Bill Barnes <billba@microsoft.com>
2024-12-08 15:59:31 -0700

    more details

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import "./App.css";
 const App = () => {
   return (
     <div id="Grid">
-      <div id="Q">Question</div>
-      <div id="A">Answer</div>
-      <div id="C">Citations</div>
-      <div id="R">Review</div>
+      <div id="Q" className="section"><h4>Question</h4></div>
+      <div id="A" className="section">
+        <h4>Answer</h4>
+        <textarea>Answer here</textarea>
+      </div>
+      <div id="C" className="section">
+        <h4>Citations</h4>
+        <ol>
+          <li className="approved">First Citation</li>
+          <li className="rejected">Second Citation</li>
+          <li className="approved">Third Citation</li>
+        </ol>
+      </div>
+      <div id="R" className="section"><h4>Review</h4></div>
     </div>
   )
 };
